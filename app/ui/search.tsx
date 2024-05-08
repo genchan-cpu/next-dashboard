904da7cc0196dba0c755dd11cddc5f4150f62062
gen <ort1031@gmail.com>
2024-05-08 22:19:40 +0900

    add pagination comp

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -13,13 +13,15 @@ export default function Search({ placeholder }: { placeholder: string }) {
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
-      params.delete('delete');
+      params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`); // pathnameは現在のパス(/dashboard/invoices)
-  }, 300);
+  }, 200);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
